337857e341536446855d019922d7fdb84ff402aa
shabnam shomail <34633069+shabnomnom@users.noreply.github.com>
2023-01-25 18:34:51 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <link rel="stylesheet" href="./style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wordle</title>
+</head>
+
+<body>
+  <form id="guessForm">
+    <input type="text" name="guess" placeholder="Enter your guess here"> </input>
+    <input type="submit"></input>
+  </form>
+
+  <table id="gameBoard">
+  </table>
+
+
+
+
+
+
+
+
+
+
+
+  <script src="./wordle.js"></script>
+</body>
+
+</html>
